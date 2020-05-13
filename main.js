e8893cc3a724047e79a28767b781567e06f840d5
Cristian Romero <crisromero02@outlook.com>
2020-05-13 10:03:12 -0700

    Removed the component folder as well as the file within it. Created a function that GETs the genere by ID from the dropdown select value and enters that in the URL of the request.

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
-var info = new movieInfo;
-info.startMovie();
+var subButt = document.querySelector('button');
+var randomPage = Math.floor(Math.random() * 500) + 1;
+var randomMovieFromPage = Math.floor(Math.random() * 20) + 1;
+
+subButt.addEventListener('click', getGenreValue);
+
+function getGenreValue(event) {
+  var dropDown = document.getElementById('genreOptions');
+  var genreID = dropDown.options[dropDown.selectedIndex].value;
+  event.preventDefault();
+
+  $.ajax({
+    method: "GET",
+    url: "https://api.themoviedb.org/3/discover/movie?api_key=" + tmdbKey + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=" + randomPage + "&with_genres=" + genreID,
+    success: handleGetSuccess,
+    error:handleGetError
+  })
+}
+
+function handleGetSuccess(success) {
+  console.log(success);
+}
+
+function handleGetError(error) {
+  console.error(error);
+}
