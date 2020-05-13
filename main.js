281c65723e0210606fbad513d9defee2d045d5ed
Cristian Romero <crisromero02@outlook.com>
2020-05-13 11:48:01 -0700

    Created function that gets the response with the movie info. I then choose a random index location and get the movie title, poster, release date and plot. I add the value of those as the text content property for p elements that I then append to their parent element.

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var subButt = document.querySelector('button');
+var asideEl = document.querySelector('aside');
+var articleEl = document.querySelector('article');
 var randomPage = Math.floor(Math.random() * 500) + 1;
 var randomMovieFromPage = Math.floor(Math.random() * 20) + 1;
 
@@ -12,15 +14,31 @@ function getGenreValue(event) {
   $.ajax({
     method: "GET",
     url: "https://api.themoviedb.org/3/discover/movie?api_key=" + tmdbKey + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=" + randomPage + "&with_genres=" + genreID,
-    success: handleGetSuccess,
-    error:handleGetError
+    success: function(data) {
+      getMovieInfo(data);
+    },
+    error: function(error) {
+      console.error(error);
+    }
   })
 }
 
-function handleGetSuccess(success) {
-  console.log(success);
-}
+function getMovieInfo(data) {
+  console.log(data.results);
+  var movieInfoResults = data.results[randomMovieFromPage];
+  console.log(movieInfoResults);
+  var moviePoster = "http://image.tmdb.org/t/p/w185/" + movieInfoResults.poster_path;
 
-function handleGetError(error) {
-  console.error(error);
+  var showMovieTitle = document.createElement('p');
+  var showMoviePlot = document.createElement('p');
+  var showMovieReleaseDate = document.createElement('p');
+  var showMoviePoster = document.createElement('img');
+
+  showMovieTitle.textContent = movieInfoResults.title;
+  showMoviePlot.textContent = movieInfoResults.overview;
+  showMovieReleaseDate.textContent = movieInfoResults.release_date;
+  showMoviePoster.src = moviePoster;
+
+  asideEl.append(showMoviePoster);
+  articleEl.append(showMovieTitle, showMovieReleaseDate, showMoviePlot);
 }
